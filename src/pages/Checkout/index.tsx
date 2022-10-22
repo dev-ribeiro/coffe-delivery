952fb8fc57e32a000925aa68648e7d549fcd076e
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-10-22 20:43:04 -0300

    config: create checkout route

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,9 @@
+import { CheckoutContainer } from "./styles";
+
+export function CheckoutPage() {
+    return (
+        <CheckoutContainer>
+            <h1>Checkout</h1>
+        </CheckoutContainer>
+    )
+}
