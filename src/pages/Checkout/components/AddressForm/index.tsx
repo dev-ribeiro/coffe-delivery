c1a0c03571b6567f4cbfc076940f017c0dd0f466
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-10-29 13:19:08 -0300

    fix: adjust form container of adress form

--- src/pages/Checkout/components/AddressForm/index.tsx
+++ src/pages/Checkout/components/AddressForm/index.tsx
@@ -5,30 +5,48 @@ import {
   MapPinLine,
   Money,
 } from 'phosphor-react'
-import { AdressFormContainer, AdressFormInfo } from './styles'
+import {
+  AdressFormContainer,
+  AdressFormInfo,
+  FormInfoAndInputsWrapper,
+  FormInputsWrapper,
+  Input,
+} from './styles'
 
 export function AddressForm() {
   return (
     <AdressFormContainer>
       <h2>Complete seu pedido</h2>
       <form>
-        <AdressFormInfo>
-          <MapPinLine size={22} />
-          <div>
-            <h3>Endereço de entrega</h3>
-            <span>Informe o endereço onde deseja receber seu pedido</span>
-          </div>
-        </AdressFormInfo>
-        <div>
-          <input type="text" placeholder="CEP" className="cep" />
-          <input type="text" placeholder="Rua" />
-          <input type="number" className="address" placeholder="Número" />
-          <input type="text" className="complement" placeholder="Complemento" />
-          {/* <label htmlFor="complement">Opcional</label> */}
-          <input type="text" className="district" placeholder="Bairro" />
-          <input type="text" className="city" placeholder="Cidade" />
-          <input type="text" className="state" placeholder="UF" />
-        </div>
+        <FormInfoAndInputsWrapper>
+          <AdressFormInfo>
+            <MapPinLine size={22} />
+            <div>
+              <h3>Endereço de entrega</h3>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </AdressFormInfo>
+          <FormInputsWrapper>
+            <Input
+              type="text"
+              placeholder="CEP"
+              inputName="cep"
+              className="cep"
+            />
+            <Input type="text" placeholder="Rua" inputName="street" />
+            <Input type="number" placeholder="Número" inputName="adress" />
+            <Input
+              type="text"
+              placeholder="Complemento"
+              inputName="complement"
+              className="complement"
+            />
+            {/* <label htmlFor="complement">Opcional</label> */}
+            <Input type="text" placeholder="Bairro" inputName="district" />
+            <Input type="text" placeholder="Cidade" inputName="city" />
+            <Input type="text" placeholder="UF" inputName="state" />
+          </FormInputsWrapper>
+        </FormInfoAndInputsWrapper>
         <div>
           <header>
             <CurrencyDollar size={22} />
--- src/pages/Checkout/components/AddressForm/styles.ts
+++ src/pages/Checkout/components/AddressForm/styles.ts
@@ -5,8 +5,15 @@ export const AdressFormContainer = styled.section`
   width: 40rem;
 `
 
+export const FormInfoAndInputsWrapper = styled.div`
+  padding: 2.5rem;
+
+  border-radius: 6px;
+  background: ${(props) => props.theme['gray-200']};
+`
+
 export const AdressFormInfo = styled.header`
-  margin: 2.5rem 0;
+  margin-bottom: 2.5rem;
 
   display: flex;
   gap: 8px;
@@ -24,5 +31,48 @@ export const AdressFormInfo = styled.header`
 
   span {
     font-size: 0.875rem;
+  }
+`
+
+const INPUTS_NAME = {
+  cep: 'cep',
+  street: 'street',
+  adress: 'adress',
+  complement: 'complement',
+  district: 'district',
+  city: 'city',
+  state: 'state',
+} as const
+
+interface InputProps {
+  inputName: keyof typeof INPUTS_NAME
+}
+
+export const FormInputsWrapper = styled.div`
+  display: grid;
+  grid-template:
+    'cep cep cep' 42px
+    'street street street' 42px
+    'adress complement complement' 42px
+    'district city state' 42px/
+    1fr 1fr 60px;
+  gap: 16px;
+
+  .cep {
+    width: 200px;
+  }
+`
+
+export const Input = styled.input<InputProps>`
+  padding: 0.75rem;
+  grid-area: ${(props) => props.inputName};
+
+  border: none;
+  border-radius: 4px;
+  background: ${(props) => props.theme['gray-300']};
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-450']};
+    font-family: 'Roboto', sans-serif;
   }
 `
