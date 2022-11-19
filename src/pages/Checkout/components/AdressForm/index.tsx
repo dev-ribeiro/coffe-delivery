3c333953e2020319f1933734783c3b3201c6e43c
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-19 07:44:31 -0300

    styles: rebuild checkout page

--- src/pages/Checkout/components/AdressForm/index.tsx
+++ src/pages/Checkout/components/AdressForm/index.tsx
@@ -1,55 +1,142 @@
-import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+/* eslint-disable array-callback-return */
+import * as RadioGroup from '@radix-ui/react-radio-group'
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react'
+import { useContext } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { CoffeeContext } from '../../../../contexts/CoffeeContext'
+import { CoffeeItemCheckout } from '../CoffeeItemCheckout'
 import {
   AdressFormContainer,
+  AdressInformationHeader,
   AdressInformationsContainer,
+  AdressInput,
   AdressInputsGroup,
+  AdressInputWrapper,
   CheckoutCartContainer,
   DeliveryPanelContainer,
   OrderSummary,
   OrderSummaryContainer,
+  PaymentHeaderContainer,
   PaymentOptionContainer,
+  PaymentOptionsWrapper,
 } from './styles'
 
 export function AdressForm() {
+  const { coffees } = useContext(CoffeeContext)
+  const { control } = useForm()
+
   return (
     <AdressFormContainer>
       <DeliveryPanelContainer>
+        <h2>Complete seu pedido</h2>
         <AdressInformationsContainer>
-          <header>
+          <AdressInformationHeader>
             <MapPinLine size={22} />
-            <h2>Endereço de entrega</h2>
-            <span>Informe o endereço onde deseja receber seu pedido</span>
-          </header>
+            <div>
+              <h2>Endereço de entrega</h2>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </AdressInformationHeader>
           <AdressInputsGroup>
-            <input placeholder="CEP" type="text" />
-            <input placeholder="Rua" type="text" />
-            <input placeholder="Número" type="text" />
-            <input placeholder="Complemento" type="text" />
-            <input placeholder="Bairro" type="text" />
-            <input placeholder="Cidade" type="text" />
-            <input placeholder="UF" type="text" />
+            <AdressInputWrapper inputGroupName="cep">
+              <AdressInput name="cep" type="text" />
+              <label htmlFor="cep">CEP</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="street">
+              <AdressInput name="street" type="text" />
+              <label htmlFor="street">Rua</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="address">
+              <AdressInput name="address" type="text" />
+              <label htmlFor="address">Número</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="complement">
+              <AdressInput name="complement" type="text" />
+              <label htmlFor="complement">Complemento</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="district">
+              <AdressInput name="district" type="text" />
+              <label htmlFor="district">Bairro</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="city">
+              <AdressInput name="city" type="text" />
+              <label htmlFor="city">Cidade</label>
+            </AdressInputWrapper>
+            <AdressInputWrapper inputGroupName="state">
+              <AdressInput name="state" type="text" />
+              <label htmlFor="state">UF</label>
+            </AdressInputWrapper>
           </AdressInputsGroup>
         </AdressInformationsContainer>
         <PaymentOptionContainer>
-          <header>
+          <PaymentHeaderContainer>
             <CurrencyDollar size={22} />
-            <h2>Pagamento</h2>
-            <span>
-              O pagamento é feito na entrega. Escolha a forma que deseja pagar
-            </span>
-          </header>
+            <div>
+              <h2>Pagamento</h2>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </PaymentHeaderContainer>
+          <Controller
+            control={control}
+            name="paymentMethod"
+            render={({ field }) => {
+              return (
+                <PaymentOptionsWrapper
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <RadioGroup.Item value="credit" asChild>
+                    <button>
+                      <CreditCard size={16} />
+                      <span>CARTÃO DE CRÉDITO</span>
+                    </button>
+                  </RadioGroup.Item>
+                  <RadioGroup.Item value="debit" asChild>
+                    <button>
+                      <Bank size={16} />
+                      <span>CARTÃO DE DÉBITO</span>
+                    </button>
+                  </RadioGroup.Item>
+                  <RadioGroup.Item value="cash" asChild>
+                    <button>
+                      <Money size={16} />
+                      <span>DINHEIRO</span>
+                    </button>
+                  </RadioGroup.Item>
+                </PaymentOptionsWrapper>
+              )
+            }}
+          />
         </PaymentOptionContainer>
       </DeliveryPanelContainer>
       <OrderSummaryContainer>
-        <CheckoutCartContainer></CheckoutCartContainer>
-        <OrderSummary>
-          <span>Total de itens</span>
-          <span>R$ 29,70</span>
-          <span>Entrega</span>
-          <span>R$ 3,50</span>
-          <h2>Total</h2>
-          <h2>R$ 33,70</h2>
-        </OrderSummary>
+        <h2>Cafés selecionados</h2>
+        <div>
+          <CheckoutCartContainer>
+            {coffees.map((coffee, index) => {
+              if (index < 2) {
+                return <CoffeeItemCheckout key={coffee.id} {...coffee} />
+              }
+            })}
+          </CheckoutCartContainer>
+          <OrderSummary>
+            <span>Total de itens</span>
+            <span>R$ 29,70</span>
+            <span>Entrega</span>
+            <span>R$ 3,50</span>
+            <span>Total</span>
+            <span>R$ 33,70</span>
+          </OrderSummary>
+          <button type="submit">CONFIRMAR PEDIDO</button>
+        </div>
       </OrderSummaryContainer>
     </AdressFormContainer>
   )
--- src/pages/Checkout/components/AdressForm/styles.ts
+++ src/pages/Checkout/components/AdressForm/styles.ts
@@ -1,17 +1,238 @@
 import styled from 'styled-components'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
-export const AdressFormContainer = styled.form``
+export const AdressFormContainer = styled.form`
+  flex: 1;
+  display: flex;
+  gap: 32px;
+`
 
-export const DeliveryPanelContainer = styled.div``
+export const DeliveryPanelContainer = styled.div`
+  margin-top: 2.5rem;
+  width: 40rem;
+  display: flex;
+  gap: 0.75rem;
+  flex-direction: column;
 
-export const AdressInformationsContainer = styled.div``
+  & > h2 {
+    font-size: 1.125rem;
+    font-family: 'Baloo 2', monospace;
+  }
+`
 
-export const AdressInputsGroup = styled.div``
+export const AdressInformationsContainer = styled.div`
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background: ${(props) => props.theme['gray-200']};
+  border-radius: 6px;
+`
 
-export const PaymentOptionContainer = styled.div``
+export const AdressInformationHeader = styled.header`
+  display: flex;
+  align-items: flex-start;
+  gap: 0.5rem;
 
-export const OrderSummaryContainer = styled.div``
+  svg {
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  div {
+    h2 {
+      font-family: 'Roboto', sans-serif;
+      font-size: 1rem;
+      font-weight: 400;
+    }
+    span {
+      color: ${(props) => props.theme['gray-500']};
+      font-size: 0.875rem;
+    }
+  }
+`
+
+const INPUTS_NAME = {
+  cep: 'cep',
+  street: 'street',
+  address: 'address',
+  complement: 'complement',
+  district: 'district',
+  city: 'city',
+  state: 'state',
+} as const
+
+interface InputWrapperProps {
+  inputGroupName: keyof typeof INPUTS_NAME
+}
+
+export const AdressInputsGroup = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template:
+    'cep cep cep cep cep' 1fr
+    'street street street street street' 1fr
+    'address address complement complement complement' 1fr
+    'district district city city state' 1fr
+    /1fr 1fr 1fr 2fr 3.75rem;
+
+  div:first-child {
+    width: 12.5rem;
+  }
+`
+
+export const AdressInputWrapper = styled.div<InputWrapperProps>`
+  grid-area: ${(props) => props.inputGroupName};
+  height: 2.625rem;
+  position: relative;
+
+  label {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0.75rem;
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['gray-450']};
+  }
+
+  .hideLabel {
+    display: none;
+  }
+`
+
+export const AdressInput = styled.input`
+  height: 100%;
+  width: 100%;
+  padding: 0.75rem;
+  height: 2.625rem;
+  border: none;
+  display: inline;
+  border: 1px solid ${(props) => props.theme['gray-350']};
+  background: ${(props) => props.theme['gray-100']};
+
+  :focus {
+    border: 1px solid ${(props) => props.theme['yellow-dark']};
+  }
+`
+
+export const PaymentOptionContainer = styled.div`
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  background: ${(props) => props.theme['gray-200']};
+  border-radius: 6px;
+`
+
+export const PaymentOptionsWrapper = styled(RadioGroup.Root)`
+  display: flex;
+  gap: 0.75rem;
+  justify-content: center;
+
+  button {
+    padding: 1rem;
+    width: 11.166875rem;
+    gap: 0.75rem;
+
+    display: flex;
+    align-items: center;
+
+    border: none;
+    border-radius: 6px;
+    background: ${(props) => props.theme['gray-350']};
+
+    svg {
+      color: ${(props) => props.theme.purple};
+    }
+
+    span {
+      font-size: 12px;
+      line-height: 160%;
+      color: ${(props) => props.theme['gray-500']};
+    }
+
+    &[data-state='checked'] {
+      border: 1px solid ${(props) => props.theme.purple};
+      background: ${(props) => props.theme['purple-light']};
+    }
+  }
+`
+
+export const PaymentHeaderContainer = styled.header`
+  display: flex;
+  gap: 0.5rem;
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+
+  div {
+    h2 {
+      margin-bottom: 2px;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 400;
+      font-size: 1rem;
+      color: ${(props) => props.theme['gray-800']};
+    }
+    span {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['gray-500']};
+    }
+  }
+`
+
+export const OrderSummaryContainer = styled.div`
+  margin-top: 2.5rem;
+  display: flex;
+  gap: 0.75rem;
+  flex-direction: column;
+
+  & > h2 {
+    font-family: 'Baloo 2', monospace;
+    font-size: 1.125rem;
+  }
+
+  & > div {
+    padding: 2.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    background: ${(props) => props.theme['gray-200']};
+
+    button[type='submit'] {
+      height: 2.875rem;
+      font-size: 0.875rem;
+      color: ${(props) => props.theme.white};
+      border: none;
+      background: ${(props) => props.theme.yellow};
+
+      &:hover {
+        background: ${(props) => props.theme['yellow-dark']};
+      }
+    }
+  }
+`
 
 export const CheckoutCartContainer = styled.div``
 
-export const OrderSummary = styled.div``
+export const OrderSummary = styled.div`
+  display: grid;
+  gap: 12px;
+  grid-template-columns: repeat(2, auto);
+  justify-content: space-between;
+  justify-items: stretch;
+
+  span {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['gray-500']};
+  }
+
+  span:nth-child(even) {
+    text-align: end;
+  }
+
+  span:nth-last-child(-n + 2) {
+    font-size: 1.25rem;
+    font-weight: bold;
+    color: ${(props) => props.theme['gray-800']};
+  }
+`
